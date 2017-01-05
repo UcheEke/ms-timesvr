8cddbf5a6e0b18ab1601300cd8aa8c55b2720883
Uche Eke <uche.eke@gmail.com>
2017-01-05 22:50:04 +0000

    Project ready for upload to Heroku

--- server.js
+++ server.js
@@ -6,7 +6,35 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function(req, res){
-    res.send('Hello World!');
+let errDateFormat = function(){
+    let output = {};
+    output['natural'] = null;
+    output['unix'] = null;
+    return output;
+};
+
+let formatOutput = function(strTime){
+    let output = {};
+    let t = Date.parse(strTime);
+    if(Number.isNaN(t)){
+       let n = parseInt(strTime);
+       if (Number.isNaN(n)){
+           return errDateFormat();
+       } else {
+           output['unix'] = n;
+           output['natural'] = new Date(n).toDateString();
+       }
+    } else {
+        output['natural'] = strTime;
+        output['unix'] = t;
+    }
+    return output;
+};
+
+app.get('/:time', function(req, res){
+    let time = req.params.time; // string
+    console.log("Time passed into request:", time);
+    let output = formatOutput(time);
+    res.send(output);
 });
 app.listen(8080);
